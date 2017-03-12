61b881fef6feef334315eb821880c6e6ab12c6a2
王山姆 <sakmor@gmail.com>
2017-03-13 00:11:08 +0800

    FirstAdd

---
+++ Assets/main.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+
+    GameObject player;              //我們操控的玩家
+    GameObject mushroom;			//香菇之王，所有的香菇都是透過複製(Instantiate)他而來的
+    public float mushroomSpeed;		//間隔多少時間產生新的香菇
+    float lastTime;					//計時用
+    int liveMushroom;				//現在存活的香菇數量
+    public int maxLiveMushroom;		//場景內存活香菇的上限值，達到此數值不再新增香菇
+
+
+    // Use this for initialization
+    void Start()
+    {
+        maxLiveMushroom = 30;
+        mushroomSpeed = 2;
+        lastTime = Time.fixedTime;
+        player = GameObject.Find("player");
+        mushroom = GameObject.Find("murshroom");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseClick();
+        mushroomfactory();
+
+    }
+    void mushroomfactory()
+    {
+        if (Time.fixedTime - lastTime > mushroomSpeed)
+        {
+            lastTime = Time.fixedTime;
+            //如果香菇數量沒有達到上限值的話...
+            if (maxLiveMushroom > liveMushroom)
+            {
+                makeNewMushroom();
+            }
+        }
+
+    }
+    void makeNewMushroom()
+    {
+        liveMushroom++;
+        GameObject makeNewMushroom = Instantiate(mushroom);
+        float randomX = UnityEngine.Random.Range(-7.5f, 7.5f);
+        float randomZ = UnityEngine.Random.Range(-6, 8);
+        makeNewMushroom.transform.position = new Vector3(randomX, 0, randomZ);
+
+    }
+    void mouseClick()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "mushroom")
+                {
+                    player.GetComponent<player>().setTargetMushroom(hit.transform.gameObject);
+                    player.GetComponent<player>().setgoPos(new Vector3(hit.point.x, 0, hit.point.z));
+                }
+
+            }
+        }
+    }
+
+}
---
+++ Assets/player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    GameObject scroeTextOBJ;
+    GameObject blackScreen;
+    int score;
+    public float speed;
+    GameObject clickMushroom;
+    Vector3 goPos;
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        blackScreen = GameObject.Find("blackScreen");
+        blackScreen.GetComponent<UnityEngine.UI.Image>().enabled = false;
+        scroeTextOBJ = GameObject.Find("scoreText");
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gotoPos(goPos);
+    }
+    public void setgoPos(Vector3 temp)
+    {
+        goPos = temp;
+    }
+    public void setTargetMushroom(GameObject temp)
+    {
+        clickMushroom = temp;
+    }
+    void gotoPos(Vector3 pos)
+    {
+        Vector3 thisPos = this.transform.position;
+        float posDist = Vector3.Distance(thisPos, pos);
+        if (posDist > 0.01f)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, pos, speed * Time.deltaTime);
+            this.GetComponent<Animator>().SetInteger("state", 1);
+        }
+        else if (clickMushroom != null)
+        {
+
+            score++;
+            scroeTextOBJ.GetComponent<UnityEngine.UI.Text>().text = score.ToString("F0");
+            Destroy(clickMushroom);
+        }
+        else
+        {
+            this.GetComponent<Animator>().SetInteger("state", 0);
+        }
+    }
+
+}
